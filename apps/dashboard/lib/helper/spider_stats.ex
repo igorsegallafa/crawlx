0f7c9c0b8ecb7b696801c854d8f95333f6c64e94
Igor Segalla <igorsegallafa@gmail.com>
2021-07-02 21:15:08 -0300

    Update spider_stats.ex

--- apps/dashboard/lib/helper/spider_stats.ex
+++ apps/dashboard/lib/helper/spider_stats.ex
@@ -11,14 +11,18 @@ defmodule Dashboard.Helper.SpiderStats do
       |> to_string()
       |> String.replace("Elixir.", "")
 
-    response_spider = HTTPoison.get!("http://localhost:4001/spiders/#{spider_name}/scheduled-requests")
-    response_url = HTTPoison.get!(spider.get_urls() |> List.first())
-
-    %{
-      name: spider_name,
-      is_running: response_spider.body != "Spider is not running",
-      web_successful: response_url.status_code >= 200 && response_url.status_code <= 226
-    }
+    with(
+      {:ok, response_spider} <- HTTPoison.get("http://localhost:4001/spiders/#{spider_name}/scheduled-requests"),
+      {:ok, response_url} <- HTTPoison.get(spider.get_urls() |> List.first())
+    ) do
+      %{
+        name: spider_name,
+        is_running: response_spider.body != "Spider is not running",
+        web_successful: response_url.status_code >= 200 && response_url.status_code <= 226
+      }
+    else
+      {:error, _} -> %{name: spider_name, is_running: false, web_successful: false}
+    end
   end
 
 end
